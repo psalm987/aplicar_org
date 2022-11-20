01af653e40ab505d54b2375d7dab780c75e67b59
samrythmn@gmail.com <samrythmn@gmail.com>
2022-11-20 11:53:10 +0100

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,61 @@
+import History from "./History";
+import CreateAccount from "./CreateAccount";
+import VerifyAcc from "./VerifyAcc";
+import Login from "./Login";
+import ErrorPage from "./ErrorPage";
+import PrivacyPolicy from "./PrivacyPolicy";
+import ContactUs from "./ContactUs";
+import Faq from "./Faq";
+import SeeAllHistory from "./SeeAllHistory";
+import Career from "./Career";
+import Pricing from "./Pricing";
+import Blog from "./Blog";
+import SingleblogPage from "./SingleblogPage";
+import ProfilePage from "./ProfilePage";
+import UploadCV from "./UploadCV";
+import Features from "./Features";
+import Preview from "./Preview";
+import PgCoverLetter from "./PgCoverLetter";
+import TermsAndCondition from "./TermsAndConditions";
+import Upload from "./upload/Upload";
+import UploadData from "./uploadData/UploadData";
+import Documentation from "./Documentation";
+import Forum from "./forum/Forum";
+import Thread from "./forum/Thread";
+import Post from "./forum/Post";
+import EmailOTP from "./EmailOTP";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import About from "./About";
+
+export {
+	CreateAccount,
+	History,
+	VerifyAcc,
+	ErrorPage,
+	Login,
+	ContactUs,
+	Faq,
+	SeeAllHistory,
+	SingleblogPage,
+	Blog,
+	Pricing,
+	ProfilePage,
+	UploadCV,
+	Features,
+	Preview,
+	PgCoverLetter,
+	TermsAndCondition,
+	Upload,
+	UploadData,
+	About,
+	PrivacyPolicy,
+	Documentation,
+	Forum,
+	Thread,
+	Post,
+	EmailOTP,
+	ForgotPassword,
+	ResetPassword,
+	Career,
+};
